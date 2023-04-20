b6a4020e47cb90a74c71d38f00afcad5edee165b
LemonWaffle <datem732@naver.com>
2023-04-21 05:25:02 +0900

    model 수정

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from app.models import User, SentenceData, UserGameInfo, Ranking, ExampleData
+
+admin.site.register(User)
+admin.site.register(SentenceData)
+#admin.site.register(UserStudyInfo)
+admin.site.register(UserGameInfo)
+admin.site.register(Ranking)
+admin.site.register(ExampleData)
--- app/models.py
+++ app/models.py
@@ -14,19 +14,25 @@ class SentenceData(models.Model) :
     blankWord = models.CharField(max_length=50)             # 빈칸 단어
     blankPos = models.IntegerField()                        # 빈칸 위치
     ansKor = models.CharField(max_length=400)               # 정답 한글 문장
-
+"""
 class UserStudyInfo(models.Model) :
-    id = models.CharField(foreign_key=True, max_length=50)  # 유저 ID
-    sentenceNum = models.IntegerField(foreign_key = True)   # 문장 번호
+    id = models.CharField(primary_key=True, max_length=50)  # 유저 ID
+    #sentenceNum = models.ForeignKey(SentenceData)          # 문장 번호
     wrongNum = models.IntegerField()                        # 틀린 횟수
     check = models.IntegerField()                           # 오답 횟수
-
+"""
 class UserGameInfo(models.Model) :
-    id = models.CharField(foreign_key=True, max_length=50)  # 유저 ID
-    sentenceNum = models.IntegerField(foreign_key = True)   # 문장 번호
+    id = models.CharField(primary_key=True, max_length=50)  # 유저 ID
+    sentenceNum = models.ForeignKey(SentenceData, on_delete=models.CASCADE)          # 문장 번호
     score = models.IntegerField()                           # 게임 점수
 
 class Ranking(models.Model) :
-    id = models.CharField(foreign_key=True, max_length=50)  # 유저 ID
+    id = models.CharField(primary_key=True, max_length=50)  # 유저 ID
     userList = models.CharField(max_length=200)             # 유저 리스트
     maxScore = models.IntegerField()                        # 게임 최고 점수
+
+class ExampleData(models.Model) :
+    sentenceNum = models.IntegerField(primary_key=True)     # 문장 번호
+    example1 = models.CharField(max_length=50)              # 보기 1번
+    example2 = models.CharField(max_length=50)              # 보기 2번
+    example3 = models.CharField(max_length=50)              # 보기 3번
